19f7b1436d8ba79705548fe72ad7eb56afe84aa8
Luís Silva <luiscomsnofim@gmail.com>
2025-02-22 23:31:54 +0100

    feat: update

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,5 @@
 import * as vscode from "vscode";
 import * as path from "path";
-import * as fs from "fs";
 
 export function activate(context: vscode.ExtensionContext) {
     const disposable = vscode.commands.registerCommand(
